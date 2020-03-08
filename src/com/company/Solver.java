7dde2629bccc0ccf8fc7a29cfae3ba7c04f981a6
denrus99 <denrus99@gmail.com>
2020-03-08 15:45:28 +0500

    Yeah, it works(i hope)

--- src/com/company/Solver.java
+++ src/com/company/Solver.java
@@ -5,15 +5,16 @@ import java.util.*;
 public class Solver implements ConundrumSolver {
     @Override
     public int[] resolve(int[] initialState) {
-        Graph model = new Graph(initialState);
+        Graph start = new Graph(initialState);
+        Graph end = new Graph(new int[] {1,2,3,4,0,5,6,7});
         Map<Graph, Graph> stateTrack = new HashMap<Graph, Graph>();
-        stateTrack.put(model, null);
+        stateTrack.put(start, null);
         Queue<Graph> queue = new ArrayDeque<Graph>();
-        queue.offer(model);
+        queue.offer(start);
         while (queue.size() != 0) {
             Graph state = queue.poll();
-            state.ShowCurrentState(state.GetCurrentState());
-            System.out.println(state.hashCode());
+            //state.ShowCurrentState(state.GetCurrentState());
+            //System.out.println(state.hashCode());
             if (state.IsSolved()) break;
             ArrayList<Graph> nextStates = state.GetNextStates();
             for (Graph nextState : nextStates) {
@@ -22,6 +23,12 @@ public class Solver implements ConundrumSolver {
                     queue.offer(nextState);
                 }
             }
+        }
+        //ArrayList result = new ArrayList();
+        while (end != null)
+        {
+            end.ShowCurrentState(end.GetCurrentState());
+            end = stateTrack.get(end);
         }
 
         return new int[0];
